d8ef3ab02c8e86885deff144c4e4b9ff2657a420
Unknown <gabrield47@gmail.com>
2016-09-27 23:15:23 -0300

    feat(CestoDeLembas): criação da classe, atributo de qtde e método que retorna boolean se a quantidade pode ser dividida entre 2

---
+++ src/modulo-01-java-OO/modulo-01/CestoDeLembas.java
@@ -0,0 +1,21 @@
+
+/**
+ * Escreva a descrição da classe CestoDeLembas aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class CestoDeLembas
+{
+   private int quantidade;
+   
+   public boolean divisao(){
+       boolean ehDivisivel;
+       if(quantidade <= 100 && quantidade >= 1){
+           ehDivisivel = (quantidade % 2 == 0)?true:false;
+        }else{
+            ehDivisivel = false;
+        }
+        return ehDivisivel;
+    }
+}
