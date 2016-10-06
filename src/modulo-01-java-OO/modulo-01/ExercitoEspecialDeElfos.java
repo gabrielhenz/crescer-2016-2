74a2ed00c4dba5e25195a6ab519b2d39d71007ca
Unknown <gabrield47@gmail.com>
2016-10-05 23:16:40 -0300

    feat(ExercitoEspecialDeElfos): classe de exercito que utiliza HashMap.
    OBS: precisei percorrer todo o HashMap para buscar elfos por Status.

---
+++ src/modulo-01-java-OO/modulo-01/ExercitoEspecialDeElfos.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+public class ExercitoEspecialDeElfos
+{
+    private HashMap<String, Elfo> elfosDoExercitoEspecial;
+    
+    public ExercitoEspecialDeElfos(){
+        elfosDoExercitoEspecial = new HashMap<String, Elfo>();
+    }
+    
+    public Elfo[] getExercitoEspecialDeElfos(){
+        ArrayList<Elfo> elfos = new ArrayList<>();
+        for(Elfo elfo : elfosDoExercitoEspecial.values()){
+            elfos.add(elfo);
+        }
+        return elfos.toArray(new Elfo[elfos.size()]);
+    }
+    
+    public void alistarElfoDoExercitoEspecial(Elfo elfo){
+        if(elfo.getClass() == ElfoNoturno.class || elfo.getClass() == ElfoVerde.class)
+            elfosDoExercitoEspecial.put(elfo.getNome(), elfo);
+    }
+    
+    public Elfo buscarPorNome(String nome){
+        return elfosDoExercitoEspecial.get(nome);
+    }
+    
+    public ArrayList<Elfo> buscarPorStatus(Status status){
+        ArrayList<Elfo> elfos = new ArrayList<>();
+        for(Elfo elfo : elfosDoExercitoEspecial.values()){
+            if(elfo.getStatus().equals(status))
+            elfos.add(elfo);
+        }
+        return elfos;
+    }
+    
+}
