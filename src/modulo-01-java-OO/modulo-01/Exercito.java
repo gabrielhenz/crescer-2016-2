093b397a299bb6daa92fd62ec6828874690775a8
Unknown <gabrield47@gmail.com>
2016-10-07 17:34:48 -0300

    feat(Exercito): adicionar throws para o método alistar.

--- src/modulo-01-java-OO/modulo-01/Exercito.java
+++ src/modulo-01-java-OO/modulo-01/Exercito.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public interface Exercito {
     Elfo[] getContingente();
-    void alistar(Elfo elfo);
+    void alistar(Elfo elfo) throws NaoPodeAlistarException;
     Elfo buscar(String nome);
     ArrayList<Elfo> buscar(Status status);
     void atacar();
