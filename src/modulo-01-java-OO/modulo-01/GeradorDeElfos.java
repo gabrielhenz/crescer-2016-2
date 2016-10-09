993b02b1aee8c22f8a6c388866d20807ae4562eb
Unknown <gabrield47@gmail.com>
2016-10-09 15:12:22 -0300

    feat(GeradorDeElfos): listas para os testas da EstrategiaLittleMumu.

--- src/modulo-01-java-OO/modulo-01/GeradorDeElfos.java
+++ src/modulo-01-java-OO/modulo-01/GeradorDeElfos.java
@@ -109,4 +109,71 @@ public final class GeradorDeElfos
                 )
              );
     }
+    
+    public static List<Elfo> retorna2ElfosVerdes1ElfoVerdeSemFlecha2ElfosNoturnosE1ElfoNoturnoMorto(){
+        ElfoNoturno noturno3 = new ElfoNoturno("noturno3");
+        noturno3.setStatus(Status.MORTO);
+        return 
+            new ArrayList<Elfo>(
+                Arrays.asList(
+                    new ElfoVerde("verde1"),
+                    new ElfoVerde("verde2"),
+                    new ElfoVerde("verde3", 0),
+                    new ElfoNoturno("noturno1"),
+                    new ElfoNoturno("noturno2"),
+                    noturno3
+                )
+             );
+    }
+    
+    public static List<Elfo> retorna2ElfosVerdes1ElfoVerdeSemFlechasE2ElfosNoturnos(){
+        return 
+            new ArrayList<Elfo>(
+                Arrays.asList(
+                    new ElfoVerde("verde1"),
+                    new ElfoVerde("verde2"),
+                    new ElfoVerde("verde3", 0),
+                    new ElfoNoturno("noturno1"),
+                    new ElfoNoturno("noturno2")
+                )
+             );
+    }
+    
+    public static List<Elfo> retorna2ElfosVerdesE1ElfosNoturno(){
+        return 
+            new ArrayList<Elfo>(
+                Arrays.asList(
+                    new ElfoVerde("verde1"),
+                    new ElfoVerde("verde2"),
+                    new ElfoNoturno("noturno1")
+                )
+             );
+    }
+    
+    public static List<Elfo> retorna2ElfosVerdes1ElfoVerdeSemFlechasE2ElfosNoturnosComQuantidadeDeFlechasDiferentes(){
+        return 
+            new ArrayList<Elfo>(
+                Arrays.asList(
+                    new ElfoVerde("verde1", 100),
+                    new ElfoVerde("verde2", 50),
+                    new ElfoVerde("verde3", 0),
+                    new ElfoNoturno("noturno1", 300),
+                    new ElfoNoturno("noturno2")
+                )
+             );
+    }
+    
+    public static List<Elfo> retorna2ElfosVerdesE1ElfosNoturnoComQuantidadeDeFlechasOrdenadasDescendente(){
+        return 
+            new ArrayList<Elfo>(
+                Arrays.asList(
+                    new ElfoVerde("verde2", 50),
+                    new ElfoVerde("verde1", 100),
+                    new ElfoNoturno("noturno1", 300)
+                )
+             );
+    }
+    
+    
+    
 }
