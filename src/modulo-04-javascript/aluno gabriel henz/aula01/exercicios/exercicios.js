9b6bf55bbcad7aa95af4fc434de06a108c25aaa6
Unknown <gabrield47@gmail.com>
2016-10-25 13:36:43 -0200

    feat(exercicios): exercicios feitos.

--- src/modulo-04-javascript/aluno gabriel henz/aula01/exercicios/exercicios.js
+++ src/modulo-04-javascript/aluno gabriel henz/aula01/exercicios/exercicios.js
@@ -56,8 +56,79 @@ console.log(find([ 1, 2, 3 ], maiorIgualADois));
 
 
 //exercicio 4 - voce sabe subtrair?
+function subtrair(num1){
+  return function(num2){
+    return num1 - num2;
+  }
+}
+console.log(subtrair(-1)(2));
+
 
 //exercicio 5 - igualdade recursiva
 function iguais(param1, param2){
-  
+  if(param1 instanceof Object && param2 instanceof Object){
+    var propriedadesParam1 = Object.getOwnPropertyNames(param1);
+    var propriedadesParam2 = Object.getOwnPropertyNames(param2);
+    if(propriedadesParam1.length != propriedadesParam2.length){
+      return false;
+    }
+    for(var i = 0, len = propriedadesParam1.length; i < len; i++){
+      var ehIgual = true;
+      if(param1[propriedadesParam1[i]] instanceof Object && param2[propriedadesParam2[i]] instanceof Object){
+        ehIgual = iguais(param1[propriedadesParam1[i]], param2[propriedadesParam2[i]]);
+      }
+      if(ehIgual){
+        var nomePropriedade = propriedadesParam1[i];
+        if(param1[nomePropriedade] !== param2[nomePropriedade]){
+          return false;
+        }
+      }
+    }
+    return true;
+  }else{
+    return param1 === param2;
+  }
 }
+
+//exercicio 6 - ctrc
+function ctrlC(obj){
+  if(obj instanceof Array){
+    return ehArray(obj);
+  }
+  function ehArray(obj){
+      var novoArray = [];
+      for(var i = 0; i < obj.length; i++){
+        novoArray.push(obj[i]);
+      }
+      return novoArray;
+  }
+  if(obj instanceof Object){
+    var propriedadesObj = Object.getOwnPropertyNames(obj);
+    var novoObj = {};
+    for(var i = 0, len = propriedadesObj.length; i < len; i++){
+      var nomePropriedade = propriedadesObj[i];
+      if(obj[nomePropriedade] instanceof Array){
+          novoObj[nomePropriedade] = ehArray(obj[nomePropriedade]);
+          continue;
+      }
+      if(obj[nomePropriedade] instanceof Object){
+        novoObj[nomePropriedade] = ctrlC(obj[nomePropriedade]);
+      }
+      novoObj[nomePropriedade] = obj[nomePropriedade];
+    }
+    return novoObj;
+  }else{
+    return obj;
+  }
+}
+var origem = { a: '1'}
+var destino = ctrlC(origem);
+console.log(destino);
+// { a: '1' }
+console.log(origem === destino);
+// false
+console.log(iguais(origem, destino));
+// true
+destino = ctrlC({ a: [ { b: '1', c: '2' }, { d: false }, { e: function() { return 3; } } ] });
+console.log(destino.a[2].e());
+// 3
