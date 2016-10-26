011762ee741aaf75548107f211f3694bd8100100
Unknown <gabrield47@gmail.com>
2016-10-26 20:40:18 -0200

    fix(aula01/exercicio7): corrigir exercicio 7.

--- src/modulo-04-javascript/aluno gabriel henz/aula01/exercicios/exercicio7.js
+++ src/modulo-04-javascript/aluno gabriel henz/aula01/exercicios/exercicio7.js
@@ -1,15 +1,10 @@
 //exercicio 7 - mesclar
-function mesclar(obj1, obj2, bool) {
-    if (bool) {
-        propriedadesObj1 = Object.getOwnPropertyNames(obj1);
-        propriedadesObj2 = Object.getOwnPropertyNames(obj2);
-        for (var i = 0; i < propriedadesObj1.length; i++) {
-            for (var j = 0; j < propriedadesObj2.length; j++) {
-                if (obj1[propriedadesObj1[i]] instanceof Object && obj2[propriedadesObj2[j]]) {
-                    Object.assign(obj1[propriedadesObj1[i]], obj2[propriedadesObj1[j]]);
-                }
-            }
-        }
-        Object.assign(obj1, obj2);
+function mesclar(obj1, obj2, bool = false) {
+  for(var propriedade in obj2){
+    if(bool && typeof obj2[propriedade] === 'object'){
+      mesclar(obj1[propriedade], obj2[propriedade], bool);
+    }else{
+      obj1[propriedade] = obj2[propriedade];
     }
+  }
 }
