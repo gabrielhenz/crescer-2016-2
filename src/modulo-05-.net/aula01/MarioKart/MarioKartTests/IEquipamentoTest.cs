2fb3d44b6c95b3f0115412af4912583b520bead0
Unknown <gabrield47@gmail.com>
2016-10-29 09:02:50 -0200

    test(IEquipamentoTest): testes para FogueteDePlutonio e terminar testes de UltraPack.

--- src/modulo-05-.net/aula01/MarioKart/MarioKartTests/IEquipamentoTest.cs
+++ src/modulo-05-.net/aula01/MarioKart/MarioKartTests/IEquipamentoTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MarioKart.Equipamentos;
+using MarioKart.Exceptions;
 
 namespace MarioKartTests
 {
@@ -16,11 +17,49 @@ namespace MarioKartTests
         }
 
         [TestMethod]
-        public void criaEquipamento()
+        public void criaEquipamentoPneusDeCouroDeDragao()
         {
             IEquipamento equipamento = new PneusDeCouroDeDragao();
 
             Assert.AreEqual(2, equipamento.BonusEquipamento);
+        }
+
+        [TestMethod]
+        public void criaEquipamentoFogueteDePlutonioComNivelEquipamentoPadrao()
+        {
+            IEquipamento equipamento = new FogueteDePlutonio();
+
+            Assert.AreEqual(2, equipamento.BonusEquipamento);
+        }
+
+        [TestMethod]
+        [ExpectedException (typeof (NivelDeEquipamentoException))]
+        public void criaEquipamentoFogueteDePlutonioComNivelEquipamentoMenorQueUm()
+        {
+            var equipamento = new FogueteDePlutonio(0);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NivelDeEquipamentoException))]
+        public void criaEquipamentoFogueteDePlutonioComNivelEquipamentoMaiorQueCinco()
+        {
+            var equipamento = new FogueteDePlutonio(6);
+        }
+
+        [TestMethod]
+        public void criaEquipamentoUltraPackComFogueteDePlutonio()
+        {
+            var equipamento = new UltraPack(new FogueteDePlutonio());
+
+            Assert.AreEqual(3, equipamento.BonusEquipamento);
+        }
+
+        [TestMethod]
+        public void criaEquipamentoUltraPackComUltraPack()
+        {
+            var equipamento = new UltraPack(new UltraPack(new FogueteDePlutonio()));
+
+            Assert.AreEqual(3, equipamento.BonusEquipamento);
         }
     }
 }
