8861fa7097b20e5040bbbed18e6ba5f2f868db1c
Unknown <gabrield47@gmail.com>
2016-10-29 08:01:49 -0200

    test(KartTests): criar testes para os novos tipos de Kart criados.

--- src/modulo-05-.net/aula01/MarioKart/MarioKartTests/KartTest.cs
+++ src/modulo-05-.net/aula01/MarioKart/MarioKartTests/KartTest.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MarioKart;
 using MarioKart.Corredores;
 using MarioKart.Equipamentos;
+using MarioKart.Karts;
 
 namespace MarioKartTests
 {
@@ -14,10 +15,46 @@ namespace MarioKartTests
         {
             var corredor = new Corredor("Yoshi", NivelDeHabilidade.Mediano);
             var kart = new Kart(corredor);
-            
+
             kart.Equipar(new PneusDeCouroDeDragao());
 
             Assert.AreEqual(10, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void criaKartDoTipoSonnarEVerificaVelocidade()
+        {
+            var kart = new Sonnar(new Corredor("Mario", NivelDeHabilidade.Profissional));
+
+            kart.Equipar(new MotorABaseDeLuva());
+
+            Assert.AreEqual(15, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void criaKartDoTipoLightComCorredorNoobEVerificaVelocidade()
+        {
+            var kart = new Light(new Corredor("Mario", NivelDeHabilidade.Noob));
+
+            Assert.AreEqual(9, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void criaKartDoTipoLightComCorredorProfissionalEVerificaVelocidade()
+        {
+            var kart = new Light(new Corredor("Mario", NivelDeHabilidade.Profissional));
+
+            Assert.AreEqual(8, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void criaKartDoTipoDinamEVerificaVelocidade()
+        {
+            var kart = new Dinam(new Corredor("Mario", NivelDeHabilidade.Profissional));
+
+            kart.Equipar(new PneusDeCouroDeDragao());
+
+            Assert.AreEqual(19, kart.Velocidade);
         }
     }
 }
