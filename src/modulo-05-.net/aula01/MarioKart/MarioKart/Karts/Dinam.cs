355b538e7f908c7ca84266057da692d10f46f324
Unknown <gabrield47@gmail.com>
2016-10-29 08:00:57 -0200

    feat(Dinam): criar tipo de Kart.

---
+++ src/modulo-05-.net/aula01/MarioKart/MarioKart/Karts/Dinam.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioKart.Corredores;
+
+namespace MarioKart.Karts
+{
+    public class Dinam : Kart
+    {
+        public Dinam(Corredor corredor) : base(corredor) { }
+
+        public override int Velocidade
+        {
+            get
+            {
+                //bonus de habilidade do corredor dobrado
+                return base.Velocidade + this.bonusHabilidadeCorredor();
+            }
+        }
+    }
+}
