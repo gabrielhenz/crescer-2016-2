6eff832d082e7f45d1907ef94068c4943763d364
Unknown <gabrield47@gmail.com>
2016-10-29 08:46:49 -0200

    feat(NivelDeEquipamentoException): exception para o FogueteDePlutonio.

---
+++ src/modulo-05-.net/aula01/MarioKart/MarioKart/Exceptions/NivelDeEquipamentoException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Exceptions
+{
+    public class NivelDeEquipamentoException : Exception {
+        public NivelDeEquipamentoException
+            (string message = "O nível do equipamento deve ser entre 1 e 5") 
+            : base(message) { }
+    }
+    
+}
