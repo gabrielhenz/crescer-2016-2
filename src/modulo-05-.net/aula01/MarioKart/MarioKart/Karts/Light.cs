5fc63e2da14e6352216c2b406c7e0165f08e2845
Unknown <gabrield47@gmail.com>
2016-10-29 08:01:10 -0200

    feat(Light): criar tipo de Kart.

---
+++ src/modulo-05-.net/aula01/MarioKart/MarioKart/Karts/Light.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioKart.Corredores;
+
+namespace MarioKart.Karts
+{
+    public class Light : Kart
+    {
+        public Light(Corredor corredor) : base(corredor) { }
+
+        public override int Velocidade
+        {
+            get
+            {
+                int velocidade = base.Velocidade;
+                NivelDeHabilidade nivelDeHabilidade = this.Corredor.NivelDeHabilidade;
+
+                if (nivelDeHabilidade.Equals(NivelDeHabilidade.Noob))
+                {
+                    velocidade += 3;
+                }
+
+                if (nivelDeHabilidade.Equals(NivelDeHabilidade.Profissional)){
+                    velocidade--;
+                }
+
+                return velocidade;
+            }
+        }
+    }
+}
