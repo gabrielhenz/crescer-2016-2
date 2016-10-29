d85bd309b0b23a27a1ead4a170b8064cec4f5375
Unknown <gabrield47@gmail.com>
2016-10-29 09:02:16 -0200

    feat(UltraPack): criar novo tipo de Equipamento. TO-DO: arrumar recursividade.
    O Equipamento UltraPack possui dentro dele um outro Equipamento.

---
+++ src/modulo-05-.net/aula01/MarioKart/MarioKart/Equipamentos/UltraPack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class UltraPack : IEquipamento
+    {
+        public UltraPack(IEquipamento equipamento)
+        {
+            this.Equipamento = UltraPackRecursivo(equipamento);
+        }
+
+        private IEquipamento UltraPackRecursivo(IEquipamento equipamento)
+        {
+            if(equipamento is UltraPack)
+            {
+                UltraPackRecursivo(equipamento.Equipamento);
+            }
+        }
+
+        private IEquipamento Equipamento;
+
+        public int BonusEquipamento
+        {
+            get
+            {
+                double bonusNestedEquipamento = this.Equipamento.BonusEquipamento;
+                int bonusTotal = Convert.ToInt32(Math.Ceiling(bonusNestedEquipamento * 1.20));
+
+                return bonusTotal;
+            }
+        }
+
+    }
+}
