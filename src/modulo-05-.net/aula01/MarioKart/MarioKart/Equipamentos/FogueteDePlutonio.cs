a8f84150c91297310d1b95e4ef50d6d80c311e3f
Unknown <gabrield47@gmail.com>
2016-10-29 08:46:27 -0200

    feat(FogueteDePlutonio): criar novo tipo de Equipamento.
    O Equipamento Foguete De Plutônio possui como propriedade um nível de equipamento, que somente pode ser entre 1 e 5, sendo que se o usuário não passar nenhum valor, o valor padrão é 2. Caso o valor passado não esteja nesse intervalo, estora a exception NivelDeEquipamentoException.

---
+++ src/modulo-05-.net/aula01/MarioKart/MarioKart/Equipamentos/FogueteDePlutonio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioKart.Exceptions;
+
+namespace MarioKart.Equipamentos
+{
+    public class FogueteDePlutonio : NivelDeEquipamentoException, IEquipamento
+    {
+        public FogueteDePlutonio(int nivelEquipamento = 2)
+        {
+            if( nivelEquipamento < 1 || nivelEquipamento > 5)
+            {
+                throw new NivelDeEquipamentoException();
+            }
+            else
+            {
+                this.NivelEquipamento = nivelEquipamento;
+            }
+        }
+
+        private int NivelEquipamento;
+
+        public int BonusEquipamento
+        {
+            get
+            {
+                return this.NivelEquipamento;
+            }
+        }
+    }
+}
