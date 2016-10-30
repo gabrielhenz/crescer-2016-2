26dbe337a0a52b8c11715478344294a30483be80
Unknown <gabrield47@gmail.com>
2016-10-29 23:47:26 -0200

    feat(SkyFusion): criar novo tipo de Kart.
    Este Kart é um Kart normal, mas ao mesmo tempo pode ser um Equipamento de outro Kart. O BonusDeEquipamento para o Kart 'pai' em que o SkyFusion estiver inserido está implementado na mesma classe SkyFusion.

---
+++ src/modulo-05-.net/aula01/MarioKart/MarioKart/Karts/SkyFusion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MarioKart.Corredores;
+using MarioKart.Equipamentos;
+
+namespace MarioKart.Karts
+{
+    public class SkyFusion : Kart, IEquipamento
+    {
+        public SkyFusion(Corredor corredor) : base(corredor)
+        {
+        }
+
+        public int BonusEquipamento
+        {
+            get
+            {
+                int bonusPorQuantidadeDeEquipamentos= this.Equipamentos.Count;
+
+                int bonusPorNivelDeHabilidade;
+                NivelDeHabilidade nivelDeHabilidade = this.Corredor.NivelDeHabilidade;
+
+                if (nivelDeHabilidade.Equals(NivelDeHabilidade.Noob))
+                {
+                    bonusPorNivelDeHabilidade = 1;
+                }
+                else if (nivelDeHabilidade.Equals(NivelDeHabilidade.Mediano))
+                {
+                    bonusPorNivelDeHabilidade = 2;
+                }
+                else
+                {
+                    bonusPorNivelDeHabilidade = 3;
+                }
+
+                int somaTotal = bonusPorQuantidadeDeEquipamentos + bonusPorNivelDeHabilidade;
+
+                if (this.Velocidade > 12)
+                {
+                    somaTotal += 5;
+                }
+
+                return somaTotal;
+            }
+        }
+
+        public override int Velocidade
+        {
+            get
+            {
+                return base.Velocidade;
+            }
+        }
+    }
+}
