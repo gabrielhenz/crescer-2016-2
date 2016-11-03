ff6c2f4625a03f3ceae7813e34cf1170d0586068
Unknown <gabrield47@gmail.com>
2016-11-03 16:34:50 -0200

    feat(Aula05): pasta com solution para o exercicio da aula 5

---
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Web/Controllers/StreetFighterController.cs
@@ -0,0 +1,109 @@
+﻿using StreetFighter.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class StreetFighterController : Controller
+    {
+        // GET: StreetFighter
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public ActionResult Salvar(FichaTecnicaModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("FichaTecnica", model);
+
+            }
+            else
+            {
+                ModelState.AddModelError("", "Ocorreu algum erro. Da uma olhada aí pls :(");
+                return View("Cadastro");
+            }
+        }
+
+        public ActionResult FichaTecnica()
+        {
+            string imagem = "~/Content/Images/blanka.png";
+
+            string nome = "Blanka";
+
+            DateTime dataNascimento = new DateTime(1966, 2, 12);
+
+            int altura = 192;
+
+            double peso = 96;
+
+            string golpesEspeciais = "Caçar, Eletricidade.";
+
+            string origem = "BR";
+
+            var model = new FichaTecnicaModel()
+            {
+                Imagem = imagem,
+                Nome = nome,
+                DataNascimento = dataNascimento,
+                Altura = altura,
+                Peso = peso,
+                Origem = origem,
+                GolpesEspeciais = golpesEspeciais
+            };
+            return View(model);
+        }
+
+        public ActionResult SobreMim()
+        {
+            string nome = "Gabriel Dias Henz";
+
+            DateTime nascimento = new DateTime(2000, 4, 8);
+
+            int altura = 185;
+
+            double peso = 79;
+
+            string escolaridade = "Ensino Médio com Técnico em Informática integrado (cursando 3º ano).";
+
+            string habilidadesEspeciais = "Comer até explodir, rir do que não tem graça.";
+
+            string gostaDe = "Sua mãe";
+
+            string desgosta = "Mimimi.";
+
+            string estiloDeLuta = "Sarcasmo.";
+
+            string origem = "Esteio, RS, Brasil, América Latina, Planeta Terra, Sistema Solar, Via Láctea, Borda do Superaglomerado Laniakea.";
+
+            string mestreEm = "Compilar sem erro, Ter sono, Evitar sono com café.";
+
+            var model = new SobreMimModel()
+            {
+                Nome = nome,
+                Nascimento = nascimento,
+                Altura = altura,
+                Peso = peso,
+                Escolaridade = escolaridade,
+                HabilidadesEspeciais = habilidadesEspeciais,
+                GostaDe = gostaDe,
+                Desgosta = desgosta,
+                EstiloDeLuta = estiloDeLuta,
+                Origem = origem,
+                MestreEm = mestreEm
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Web/Models/SobreMimModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StreetFighter.Web.Models
+{
+    public class SobreMimModel
+    {
+        public String Nome { get; set; }
+
+        public DateTime Nascimento { get; set; }
+
+        public int Altura { get; set; }
+
+        public Double Peso { get; set; }
+
+        public String Escolaridade { get; set; }
+
+        public String HabilidadesEspeciais { get; set; }
+
+        public String GostaDe { get; set; }
+
+        public String Desgosta { get; set; }
+
+        public String EstiloDeLuta { get; set; }
+
+        public String Origem { get; set; }
+
+        public String MestreEm { get; set; }
+    }
+}
