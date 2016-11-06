c0379796177fad582c04dc2e531a42b4527d4526
Unknown <gabrield47@gmail.com>
2016-11-06 18:35:51 -0200

    feat(PersonagemRepositorio): adicionar método para ler arquivo, salvar no arquivo e editar e excluir personagem.

--- src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Persistencia/PersonagemRepositorio.cs
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Persistencia/PersonagemRepositorio.cs
@@ -1,6 +1,7 @@
 ﻿using StreetFighter.Dominio;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,24 +10,101 @@ namespace StreetFighter.Repositorio
 {
     public class PersonagemRepositorio : IPersonagemRepositorio
     {
+        const string path = @"c:\temp\ListaDePersonagens.txt";
+        public List<Personagem> Personagens { get; private set; } = new List<Personagem>();
+
+        public PersonagemRepositorio()
+        {         
+            if (!System.IO.File.Exists(path))
+            {
+                using (System.IO.File.Create(path)) { }
+            }
+            LerPersonagens();
+        }
+
+        private void LerPersonagens()
+        {
+            var personagens = File.ReadAllLines(path);
+            foreach (var personagem in personagens)
+            {
+                string[] atributosPersonagem = personagem.Split(';');
+                if (atributosPersonagem.Length == 9)
+                {
+                    Personagens.Add(
+                        new Personagem(
+                            Convert.ToInt32(atributosPersonagem[0]),
+                            atributosPersonagem[1],
+                            atributosPersonagem[2],
+                            Convert.ToDateTime(atributosPersonagem[3]),
+                            Convert.ToInt16(atributosPersonagem[4]),
+                            Convert.ToDouble(atributosPersonagem[5]),
+                            atributosPersonagem[6],
+                            atributosPersonagem[7],
+                            Convert.ToBoolean(atributosPersonagem[8]))
+                    );
+                }
+                else
+                {
+                    Personagens.Add(
+                        new Personagem(
+                            Convert.ToInt32(atributosPersonagem[0]),
+                            "",
+                            atributosPersonagem[1],
+                            Convert.ToDateTime(atributosPersonagem[2]),
+                            Convert.ToInt16(atributosPersonagem[3]),
+                            Convert.ToDouble(atributosPersonagem[4]),
+                            atributosPersonagem[5],
+                            atributosPersonagem[6],
+                            Convert.ToBoolean(atributosPersonagem[7])
+                    ));
+                }
+            }
+        }
+
         public List<Personagem> ListarPersonagens(string filtroNome)
         {
-            throw new NotImplementedException();
+            if (!String.IsNullOrEmpty(filtroNome))
+                return Personagens.Where(p => p.Nome.Contains(filtroNome)).ToList();
+            else
+                return Personagens;
         }
 
         public void IncluirPersonagem(Personagem personagem)
         {
-            throw new NotImplementedException();
+            Personagens.Add( new Personagem((Personagens.Count > 0 ? Personagens.Last().Id + 1 : 1), 
+                (personagem.Imagem != null ? personagem.Imagem : ""), 
+                personagem.Nome, personagem.DataNascimento, personagem.Altura, personagem.Peso, personagem.Origem,
+                personagem.GolpesEspeciais, personagem.PersonagemOculto));
+            File.AppendAllText(path, Personagens.Last().ToString() + Environment.NewLine);
         }
 
         public void EditarPersonagem(Personagem personagem)
         {
-            throw new NotImplementedException();
+            int indexPersonagemAntigo = Personagens.IndexOf(Personagens.Find(p => p.Id == personagem.Id)) + 1;
+            Personagens[indexPersonagemAntigo] = personagem;
+            File.WriteAllText(path, String.Empty);
+            int qtdePersonagens = Personagens.Count;
+            string[] personagens = new string[qtdePersonagens];
+            for (int i = 0; i < qtdePersonagens; i++)
+            {
+                personagens[i] = Personagens[i].ToString();
+            }
+            File.WriteAllLines(path, personagens);
         }
 
         public void ExcluirPersonagem(Personagem personagem)
         {
-            throw new NotImplementedException();
+            Personagens.Remove(personagem);
+            File.WriteAllText(path, String.Empty);
+            int qtdePersonagens = Personagens.Count;
+            string[] personagens = new string[qtdePersonagens];
+            for (int i = 0; i < qtdePersonagens; i++)
+            {
+                personagens[i] = Personagens[i].ToString();
+            }
+            File.WriteAllLines(path, personagens);
         }
+
+        
     }
 }
