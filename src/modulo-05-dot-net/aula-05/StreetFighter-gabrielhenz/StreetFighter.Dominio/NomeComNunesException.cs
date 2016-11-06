d38c681411a519a7bda7cdee8532a4914a032f17
Unknown <gabrield47@gmail.com>
2016-11-06 18:49:37 -0200

    feat(NomeComNunesException): adicionar excessão caso o nome contenha "Nunes".

---
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Dominio/NomeComNunesException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Dominio
+{
+    public class NomeComNunesException : Exception
+    {
+        public NomeComNunesException (string mensgagem = "Não é permitido cadastrar persongens overpowered.") 
+            : base(mensgagem) { }
+    }
+}
