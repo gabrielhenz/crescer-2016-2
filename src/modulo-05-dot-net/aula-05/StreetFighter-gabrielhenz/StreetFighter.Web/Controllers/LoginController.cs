2db762c4058cc6dec8fdfa86aebf2847515c648b
Unknown <gabrield47@gmail.com>
2016-11-07 17:57:49 -0200

    feat(LoginController): adicionar action para fazer login.

---
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Web/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using StreetFighter.Aplicativo;
+using StreetFighter.Dominio;
+using StreetFighter.Web.Models;
+using StreetFighter.Web.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StreetFighter.Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult FazerLogin(string login, string senha)
+        {
+            Usuario usuarioAutenticado = ServicoDeUsuario.BuscarUsuarioAutenticado(
+                    login, senha);
+
+            if (usuarioAutenticado != null)
+            {
+                ServicoDeAutenticacao.Autenticar(new UsuarioLogadoModel(usuarioAutenticado.Nome));
+                return RedirectToAction("Index", "StreetFighter");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
