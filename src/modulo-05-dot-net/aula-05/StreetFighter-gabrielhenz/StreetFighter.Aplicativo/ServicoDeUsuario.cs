db2f2cfafa665238ca018d149c5e699647a3d551
Unknown <gabrield47@gmail.com>
2016-11-07 17:46:49 -0200

    feat(ServicoDeUsuario): método com busca de usuario autenticado.

---
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Aplicativo/ServicoDeUsuario.cs
@@ -0,0 +1,24 @@
+﻿using StreetFighter.Dominio;
+using StreetFighter.Infra;
+using StreetFighter.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Aplicativo
+{
+    public class ServicoDeUsuario
+    {
+        public static Usuario BuscarUsuarioAutenticado(string login, string senha)
+        {
+
+            string senhaDeComparacao = ServicoDeCriptografia.ConverterParaMD5($"{login}_$_{senha}");
+
+            var repositorio = new UsuarioRepositorio();
+
+            return repositorio.BuscarUsuarioAutenticado(login, senhaDeComparacao);
+        }
+    }
+}
