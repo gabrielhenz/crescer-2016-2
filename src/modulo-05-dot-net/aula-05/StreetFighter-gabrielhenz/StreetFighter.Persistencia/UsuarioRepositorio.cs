9b5cce79175ba7f0d80f6fcd5c2d541dad6914db
Unknown <gabrield47@gmail.com>
2016-11-07 17:55:49 -0200

    feat(UsuarioRepositorio): adicionar metodo para buscar usuario no banco.

---
+++ src/modulo-05-dot-net/aula-05/StreetFighter-gabrielhenz/StreetFighter.Persistencia/UsuarioRepositorio.cs
@@ -0,0 +1,54 @@
+﻿using StreetFighter.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.Persistencia
+{
+    public class UsuarioRepositorio
+    { 
+        public Usuario BuscarUsuarioAutenticado(string login, string senha)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["StreetFighterConnection"].ConnectionString;
+
+            using(var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Usuarios WHERE Login = @param_login AND Senha = @param_senha;";
+
+                var command = new SqlCommand(sql, connection);
+
+                command.Parameters.Add(new SqlParameter("param_login", login));
+                command.Parameters.Add(new SqlParameter("param_senha", senha));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    return ConverterReaderParaUsuario(reader);
+                }
+
+                connection.Close();
+            }
+
+            return null;
+        }
+
+        private Usuario ConverterReaderParaUsuario(SqlDataReader reader)
+        {
+                Usuario found = new Usuario(
+                    id: Convert.ToInt32(reader["Id"]),
+                    login: reader["Login"].ToString(),
+                    senha: reader["Senha"].ToString(),
+                    nome: reader["Nome"].ToString()
+                    );
+            return found;
+        }
+    }
+}
