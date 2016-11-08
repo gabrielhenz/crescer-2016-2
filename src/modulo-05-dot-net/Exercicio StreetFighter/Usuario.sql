00feb14336cab40299ebed5d1642627cf5a53b2f
Unknown <gabrield47@gmail.com>
2016-11-08 17:20:33 -0200

    chore(Usuario.sql): excluir script da criação da tabela Usuario.

--- src/modulo-05-dot-net/Exercicio StreetFighter/Usuario.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Usuarios(
-	Id int identity not null primary key,
-	Login varchar(255) not null unique,
-	Senha varchar(255) not null,
-	Nome varchar(255) not null
-);
-
-INSERT INTO Usuarios VALUES ('gabrieldhenz', '3f0a0f668ac920694c3e4d31fcaa350a', 'Gabriel Henz');
