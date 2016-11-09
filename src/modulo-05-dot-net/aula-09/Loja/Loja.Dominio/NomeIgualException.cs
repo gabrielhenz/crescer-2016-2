66c68cf2f648cd950a1efa77431d46656458cc98
Unknown <gabrield47@gmail.com>
2016-11-09 17:45:07 -0200

    feat(NomeIgualException): exception para quando o nome do produto já existir.

---
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/NomeIgualException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio
+{
+    public class NomeIgualException : Exception
+    {
+        public NomeIgualException(string message) : base (message) { }
+    }
+}
