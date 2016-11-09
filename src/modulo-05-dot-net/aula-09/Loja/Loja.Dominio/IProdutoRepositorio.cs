7b241c68c26dfe268a9018f45fa166c2b7a8410b
Unknown <gabrield47@gmail.com>
2016-11-09 17:43:49 -0200

    feat(IProdutoRepositorio): adicionar interface no dominio para o repositório de produto.

---
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/IProdutoRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio
+{
+    public interface IProdutoRepositorio
+    {
+        List<Produto> ListarProdutos();
+        List<Produto> BuscarPorNome(string nome);
+        void Criar(Produto produto);
+        void Editar(Produto produto);
+        void Excluir(Produto produto);
+    }
+}
