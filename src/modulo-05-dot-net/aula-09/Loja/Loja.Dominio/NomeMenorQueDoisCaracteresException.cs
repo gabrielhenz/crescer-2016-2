09053c9eed955495d797de98171a615e92d07af7
Unknown <gabrield47@gmail.com>
2016-11-09 17:45:45 -0200

    feat(NomeMenorQueDoisCaracteresException): exception para quando o nome for menor que dois caracteres.

---
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/NomeMenorQueDoisCaracteresException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio
+{
+    public class NomeMenorQueDoisCaracteresException : Exception
+    {
+        public NomeMenorQueDoisCaracteresException(string message) : base (message) { }
+    }
+}
