d7b14bbd855d5c89f9add0a12030b67eff5bd1bf
Unknown <gabrield47@gmail.com>
2016-11-10 16:45:34 -0200

    feat(Usuario): adicionar construtores.

--- src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/Usuario.cs
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/Usuario.cs
@@ -8,6 +8,19 @@ namespace Loja.Dominio
 {
     public class Usuario
     {
+        public Usuario() { }
+
+        public Usuario(int id, string email, string senha) : this(email, senha)
+        {
+            this.Id = id;
+        }
+
+        public Usuario(string email, string senha)
+        {
+            this.Email = email;
+            this.Senha = senha;
+        }
+
         public int Id { get; set; }
 
         public string Email { get; set; }
