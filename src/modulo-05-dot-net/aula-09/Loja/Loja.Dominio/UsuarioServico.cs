efdc84384a5139922f8563e78168e95d7e45138c
Unknown <gabrield47@gmail.com>
2016-11-10 16:46:34 -0200

    feat(UsuarioServico): adicionar condições para as exceptions e método para excluir usuário e buscar por id.

--- src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/UsuarioServico.cs
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/UsuarioServico.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Loja.Dominio.Exceptions.Usuario;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,12 +24,37 @@ namespace Loja.Dominio
 
             string senhaCriptografada = this.servicoCriptografia.Criptografar(senha);
 
-            if(usuarioEncontrado != null && usuarioEncontrado.Senha.Equals(senhaCriptografada))
+            if (usuarioEncontrado != null && usuarioEncontrado.Senha.Equals(senhaCriptografada))
             {
                 return usuarioEncontrado;
             }
 
             return null;
+        }
+
+        public void Salvar(Usuario usuario)
+        {
+            if (usuario.Email.Length <= 5)
+                throw new EmailMenorOuIgualACincoCaracteresException("O email precisa conter mais que cinco caracteres.");
+            if (!usuario.Email.Contains("@"))
+                throw new NaoContemArrobaException("O email precisa conter \"@\".");
+            if (usuarioRepositorio.BuscarPorEmail(usuario.Email) != null)
+                throw new EmailJaExistenteException("Este email já está cadastrado.");
+
+            if(usuario.Id == 0)
+                usuarioRepositorio.Criar(usuario);
+            else
+                usuarioRepositorio.Editar(usuario);
+        }
+
+        public void Excluir(Usuario usuario)
+        {
+            usuarioRepositorio.Excluir(usuario);
+        }
+
+        public Usuario BuscarPorId(int id)
+        {
+            return usuarioRepositorio.BuscarPorId(id);
         }
     }
 }
