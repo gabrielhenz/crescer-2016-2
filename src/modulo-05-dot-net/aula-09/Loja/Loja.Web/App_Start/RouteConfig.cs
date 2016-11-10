c1a344f47637f81d74bc66e3ba85fa75f4bd5606
Unknown <gabrield47@gmail.com>
2016-11-10 17:28:34 -0200

    fix(RouteConfig): alterar controller default da rota.

--- src/modulo-05-dot-net/aula-09/Loja/Loja.Web/App_Start/RouteConfig.cs
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Loja.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Usuario", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
