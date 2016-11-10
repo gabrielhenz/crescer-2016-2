1eeefea62f74218661d256375b61554cb46438d5
Unknown <gabrield47@gmail.com>
2016-11-10 17:36:45 -0200

    feat(UsuarioModel): adicionar model com extension methods.

---
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Web/Models/UsuarioModel.cs
@@ -0,0 +1,37 @@
+﻿using Loja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Loja.Web.Models
+{
+    public class UsuarioModel
+    {
+
+        public int? Id { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "O endereço de email não é válido.")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(30, MinimumLength = 6, ErrorMessage = "A senha deve conter no minimo 6 e no máximo 30 caracteres.")]
+        public string Senha { get; set; }
+    }
+
+    public static class UsuarioModelExtensions
+    {
+        public static UsuarioModel ConverterParaModel(this Usuario usuario)
+        {
+            return new UsuarioModel()
+            {
+                Id = usuario.Id,
+                Email = usuario.Email,
+                Senha = usuario.Senha
+            };
+        }
+    }
+}
