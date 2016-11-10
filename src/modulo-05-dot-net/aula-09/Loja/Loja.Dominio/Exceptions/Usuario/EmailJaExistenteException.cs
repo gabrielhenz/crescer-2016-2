93e14db140bb7374cbf755321ee705ce7d84714c
Unknown <gabrield47@gmail.com>
2016-11-10 17:12:47 -0200

    feat(Exceptions/Usuario): adicionar exceptions de Usuario.

---
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/Exceptions/Usuario/EmailJaExistenteException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio.Exceptions.Usuario
+{
+    public class EmailJaExistenteException : Exception
+    {
+        public EmailJaExistenteException(string message) : base(message) { }
+    }
+}
---
+++ src/modulo-05-dot-net/aula-09/Loja/Loja.Dominio/Exceptions/Usuario/EmailMenorOuIgualACincoCaracteresException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja.Dominio.Exceptions.Usuario
+{
+    public class EmailMenorOuIgualACincoCaracteresException : Exception
+    {
+        public EmailMenorOuIgualACincoCaracteresException (string message) : base (message) { }
+    }
+}
