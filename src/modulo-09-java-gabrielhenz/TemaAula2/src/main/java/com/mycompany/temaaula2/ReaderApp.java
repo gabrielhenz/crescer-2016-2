d859e947eff63582bfc011cfb6e2111cb6acb17a
Gabriel Henz <gabrield47@gmail.com>
2016-11-27 18:58:40 -0200

    feat(ReaderApp): aplicação de console para MeuReaderUtils.

---
+++ src/modulo-09-java-gabrielhenz/TemaAula2/src/main/java/com/mycompany/temaaula2/ReaderApp.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.temaaula2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class ReaderApp {
+     public static void main(String[] args) {
+        Scanner t = new Scanner(System.in);
+        MeuReaderUtils mru = new MeuReaderUtils();
+        System.out.println("Digite 'exit' para sair do programa.");
+        System.out.println("Somente são aceitos arquivos '.txt'.");
+        while(true){
+            System.out.print("Digite um arquivo: ");
+            String comando = t.nextLine();
+            if(comando.equals("exit")){
+                System.out.println("saindo...");
+                break;
+            }
+            mru.exibirConteudo(comando);
+        }
+    }
+}
